a28e54b979771ddbb0691946818febdd230f2d4a
Kary Martinez <karyskywalker@gmail.com>
2019-03-14 01:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  return 'I can if (string.toLowerCase());
+  return "I can\'t hear you!" if string.toLowerCase();
 }
